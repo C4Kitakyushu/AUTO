112b67010e038d56c2dade2199575c60cb57d198
C4Kitakyushu <156558807+C4Kitakyushu@users.noreply.github.com>
2024-04-25 23:22:49 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -4,7 +4,7 @@ module.exports.config = {
     name: "ai",
     version: "1.0.0",
     hasPermission: 0,
-    credits: "api by jerome",//api by jerome
+    credits: "Cici
     description: "Gpt architecture",
     usePrefix: false,
     commandCategory: "GPT4",
@@ -22,9 +22,9 @@ module.exports.run = async function ({ api, event, args }) {
         }
 
         if (!prompt) {
-            return api.sendMessage('Please provide a prompt to generate a text response.\nExample: ai What is the meaning of life?', event.threadID, messageID);
+            return api.sendMessage('𝘏𝘌𝘓𝘓𝘖 𝘊𝘐𝘊𝘐 𝘐𝘚 𝘈𝘓𝘐𝘝𝘌 , 𝘒𝘐𝘕𝘋𝘓𝘠 𝘗𝘙𝘖𝘝𝘐𝘋𝘌 𝘘𝘜𝘌𝘚𝘛𝘐𝘖𝘕 𝘛𝘖 𝘔𝘌(⁠≧⁠▽⁠≦⁠)', event.threadID, messageID);
         }
-        api.sendMessage('🔍 Searching for an answer to your question...', event.threadID);
+        api.sendMessage(' 𝘈𝘯𝘴𝘸𝘦𝘳𝘪𝘯𝘨 𝘺𝘰𝘶𝘳 𝘲𝘶𝘦𝘴𝘵𝘪𝘰𝘯 𝘸𝘢𝘪𝘵 𝘢 𝘴𝘦𝘤𝘰𝘯𝘥...', event.threadID);
 
         // Delay
         await new Promise(resolve => setTimeout(resolve, 2000)); // Adjust the delay time as needed
@@ -37,7 +37,7 @@ module.exports.run = async function ({ api, event, args }) {
             const generatedText = response.data.response;
 
             // Ai Answer Here
-            api.sendMessage(`🤖 Ai Answer\n━━━━━━━━━━━━━━━━\n\n𝗔𝗻𝘀𝘄𝗲𝗿: ${generatedText}\n\n━━━━━━━━━━━━━━━━`, event.threadID, messageID);
+            api.sendMessage(`🧚 | 𝖢𝗂𝖼𝗂 𝖠𝗂 🪄\n━━━━━━━━━━━━━━━━${generatedText}\n\n━━━━━━━━━━━━━━━━`, event.threadID, messageID);
         } else {
             console.error('API response did not contain expected data:', response.data);
             api.sendMessage(`❌ An error occurred while generating the text response. Please try again later. Response data: ${JSON.stringify(response.data)}`, event.threadID, messageID);
